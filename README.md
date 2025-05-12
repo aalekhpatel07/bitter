55ea9df8f4f4e58c68e392f034e8b7b73a43508e
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2025-05-11 21:35:21 -0500

    Add README.md
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# bitter
+
+Provides an iterator struct `BitIterator`, and a trait `IterBits` that lets a type adapt into the iterator that yields chunks of bits of a given size from a byte stream.
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+//! This crate provides an iterator, and a trait that lets a 
+//! type adapt into an iterator over arbitrarily sized chunks 
+//! of bits from a stream of bytes.
+//! 
+//! See [IterBits], [BitIterator], and [Bits].
+
 mod iterbits;
 pub use iterbits::{BitIterator, Bits, IterBits};
